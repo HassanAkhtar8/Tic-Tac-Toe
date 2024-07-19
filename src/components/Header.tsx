0756d48acbaaa78213c0f8e6b8cf31e126cf7d5e
Hassan Akhtar <hassanakhtar@Hassans-MacBook-Air.local>
2024-07-19 15:57:49 +0500

    Changed favicon icon and added component folder and tailwind

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+
+
+export default function Header(){
+
+    return(
+        <header className="w-full flex-col p-6 bg-lime-400">
+            <h1 className=" w-max text-3xl font-bold text-zinc-600 mr-auto ml-auto">Tic Tac Toe</h1>  
+            <p className="w-max mr-auto ml-auto text-lg font-semibold">You dare challenge me hooman?</p> 
+        </header>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
